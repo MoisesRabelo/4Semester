e78d79d12cec8e4c6950bb88470287d0e4714bd2
Moises Moura Rabelo <40120666+MoisesRabelo@users.noreply.github.com>
2019-08-02 11:57:22 -0300

    Update main.py

--- InsertionSort/main.py
+++ InsertionSort/main.py
@@ -2,41 +2,49 @@ import timeit
 from random import randint
 import matplotlib.pyplot as plt
 
-def geraVetor(tam):
-    vetor = []
-    while len(vetor) < tam:
-        n = randint(1,1*tam)
-        if n not in vetor: vetor.append(n)
-    return vetor
 
-def drawGraph(x,lista,xl = "Entradas", yl = "Y",nam="out"):
-    plt.plot(x,lista, label = "Insertion Sort")
-    plt.legend(bbox_to_anchor=(1, 1),bbox_transform=plt.gcf().transFigure)
+def geraLista(tam):
+    lista = []
+    while tam:
+        lista.append(tam)
+        tam-=1
+    return lista
+
+
+def drawGraph(x,BSort,xl = "Entradas", yl = "loops",nam="img"):
+    fig = plt.figure(figsize=(10, 13))
+    ax = fig.add_subplot(111)
+    ax.plot(x,BSort, label = "Bubble Sort")
+    ax.legend(bbox_to_anchor=(1, 1),bbox_transform=plt.gcf().transFigure)
     plt.ylabel(yl)
     plt.xlabel(xl)
     plt.savefig(nam)
 
-graf_operacoes =[]
 
-def insertionSort(vetor): 
+def bubbleSort(arr):
     count = 0
-    for i in range(1, len(vetor)):   
-        key = vetor[i] 
-        j = i-1
-        while j >=0 and key < vetor[j] : 
-                vetor[j+1] = vetor[j] 
-                j -= 1
-                count+=1
-        vetor[j+1] = key
-    graf_operacoes.append(count)
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            count+=1
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return count
+  
+  
+vetor = [10000,20000,50000,100000]
+saidaB = []
+saidaL = []
 
-quant = [10000, 20000, 50000, 100000]
-graf_tempo = []
 
-for i in range(len(quant)):
-    print(quant[i])
-    lista = geraVetor(quant[i])
-    graf_tempo.append(timeit.timeit("insertionSort({})".format(lista),setup="from __main__ import insertionSort",number=1))
+for i in range(len(vetor)):
+  saidaB.append(timeit.timeit("bubbleSort({})".format(geraLista(vetor[i])),setup="from __main__ import geraLista,bubbleSort",number=1))
 
-drawGraph(quant,graf_tempo,"Tamanho", "Tempo", "time")
-drawGraph(quant,graf_operacoes , "Tamanho", "Operações", "operacoes")
+
+drawGraph(vetor,saidaB,nam="time")
+
+
+for i in range(len(vetor)):
+  saidaL.append(bubbleSort(geraLista(vetor[i])))
+
+drawGraph(vetor,saidaL,nam="cont")
